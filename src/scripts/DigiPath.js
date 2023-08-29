49a861bbe9d0d31beb1dabbf20bc0d531a951972
Tales de Lima Filho <92755002+CapoteDoAgreste@users.noreply.github.com>
2023-08-29 15:43:51 -0300

    Comportament fix
    removing inifinite looping

--- src/scripts/DigiPath.js
+++ src/scripts/DigiPath.js
@@ -1,14 +1,11 @@
-import DigiDb from '../mocks/DigiDB';
+import DigiDb from "../mocks/DigiDB";
 
 const database = new DigiDb().digimonData;
 let path = [];
-let digiCache = [];
-let found = false;
-let newFinal = '';
 
 function getID(digiName) {
-  const {digimons} = database;
-  const ID = digimons.findIndex(digimon => digimon.name === digiName);
+  const { digimons } = database;
+  const ID = digimons.findIndex((digimon) => digimon.name === digiName);
   return ID;
 }
 
@@ -16,40 +13,31 @@ function getName(digiID) {
   return database.digimons[digiID].name;
 }
 
-function verifyCurrentLine(digiID, initial, final) {
-  let found_ = false;
+function findPath(initialDigimon, finalDigimon, exceptions) {
+  const initialID = getID(initialDigimon);
+  const finalID = getID(finalDigimon);
 
-  const {from, into} = database.digimons[digiID];
+  const queue = [{ digimonID: initialID, path: [initialDigimon] }];
+  const visited = new Set();
 
-  found_ = from.includes(final);
-  if (!found_) found_ = into.includes(final);
+  while (queue.length > 0) {
+    const { digimonID, path } = queue.shift();
+    visited.add(digimonID);
 
-  if (found_) {
-    digiName = getName(digiID);
-    console.log(digiName);
-    path.push(digiName);
-    newFinal = digiName;
-    if (digiName == initial) found = true;
-  } else {
-    digiCache = digiCache.concat(from);
-    digiCache = digiCache.concat(into);
-  }
+    if (digimonID === finalID) {
+      return path;
+    }
 
-  return found_;
-}
+    const { from, into } = database.digimons[digimonID];
 
-export default function DigiPath(initialDigimon, finalDigimon, exceptions) {
-  newFinal = finalDigimon;
-  digiCache.push(initialDigimon);
-
-  while (!found) {
-    digiCache.forEach(seekedDigimon => {
-      console.log(seekedDigimon);
-      verifyCurrentLine(getID(seekedDigimon), initialDigimon, newFinal);
+    [...from, ...into].forEach((relation) => {
+      if (!visited.has(getID(relation)) && !exceptions.includes(relation)) {
+        queue.push({ digimonID: getID(relation), path: [...path, relation] });
+      }
     });
   }
 
-  path.push(finalDigimon);
-
-  return path;
+  return null; // No path found
 }
+
+export default findPath;
